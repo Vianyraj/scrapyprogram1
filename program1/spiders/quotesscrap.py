1c12970f14c996faefe7767aae81c85c9642709d
Vinay Krishnamurthy <vinay.krishnamurthy@stud.th-deg.de>
2020-04-19 12:44:17 +0200

    demoquotesprogram

---
+++ program1/spiders/quotesscrap.py
@@ -0,0 +1,11 @@
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name='quote'
+    start_urls= [
+        'http://www.wikicfp.com/cfp/'
+    ]
+
+    def parse(self,response):
+        title = response.css('title').extract()
+        yield {'titletext': title}
